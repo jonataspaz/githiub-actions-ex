76e9ccb3411df295265d7febb06fa1d693a36086
Jonatas Paz <jopaz9@gmail.com>
2023-06-28 19:13:44 +1200

    add docker server files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+// Constants
+const PORT = 3000;
+const HOST = "127.0.0.1";
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(PORT, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
